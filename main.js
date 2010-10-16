4216ee5283569a4300b9c448e0959de5a4f5b9a2
Julien Biezemans <jb@jbpros.com>
2010-10-16 16:27:56 +0200

    Create NodeciServer

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-var http = require('http');
-var spawn = require('child_process').spawn;
-
-http.createServer(function (req, res) {
-  console.log("request");
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("starting process...");
-  
-  command = spawn('/bin/bash', ['/Users/jbpros/Projects/valipat/bash_cukes']);  
-
-  command.stdout.on('data', function (data) {
-    res.write(data);
-  });
-  
-  command.stderr.on('data', function (data) {
-    res.write(data);
-  });
-
-  command.on('exit', function (code, signal) {
-    res.write('child process return ' + code) + "\n";
-    res.end('done.\n');
-  });
-  
-}).listen(8124, "127.0.0.1");
+require.paths.unshift('./lib');
+var NodeciServer = require('nodeci_server');
+new NodeciServer();
